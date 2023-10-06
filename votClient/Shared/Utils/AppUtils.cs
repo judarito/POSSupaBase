61c81df513184f6c67e8b8e481f088f0fe1d66dd
Juan David Ricardo Torrez <jricardo@facturecolombia.com>
2023-10-06 15:17:38 -0500

    Add new lider

---
+++ votClient/Shared/Utils/AppUtils.cs
@@ -0,0 +1,10 @@
+﻿using Radzen;
+
+namespace votClient.Shared.Utils
+{
+    public static class AppUtils
+    {
+        public async static Task<bool> ShowConfirm(string Title, string Message, DialogService dialogService) => (bool)await dialogService.Confirm(Message, Title, new ConfirmOptions() { OkButtonText = "Si", CancelButtonText = "No" });
+        public  static void ShowAlert(string Title, string Message, DialogService dialogService) =>  dialogService.Alert(Message, Title, new AlertOptions() { OkButtonText = "Ok" });
+    }
+}
