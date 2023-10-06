255db97403e760211e6b6b67f42af7aa4726036d
Juan David Ricardo Torrez <jricardo@facturecolombia.com>
2023-10-05 21:54:12 -0500

    Add Api .net

---
+++ votClient/Models/Lider.cs
@@ -0,0 +1,12 @@
+﻿namespace votClient.Models
+{
+    public class Lider
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string telefono { get; set; }
+        public string cedula { get; set; }
+        public string referido { get; set; }
+
+    }
+}
