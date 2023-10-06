ee7be377852808518f766fe1ab5c9ada0a219474
Juan David Ricardo Torrez <jricardo@facturecolombia.com>
2023-10-06 09:07:54 -0500

    Add Radzen

--- votClient/Program.cs
+++ votClient/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 using votClient;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://vot20231005162706.azurewebsites.net/api/") });
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
 
 await builder.Build().RunAsync();
